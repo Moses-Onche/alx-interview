6fe114295125549ab435ac68d277607b06d1b65e
Moses-Onche <onchemoses38@gmail.com>
2023-08-18 01:07:46 +0100

    Rotate a 2D matrix in place

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a function that rotates a 2D matrix"""
 
+
 def rotate_2d_matrix(matrix):
     """
        Rotates a 2D matrix 90 degrees clockwise
@@ -12,15 +13,14 @@ def rotate_2d_matrix(matrix):
     right = len(matrix) - 1
 
     while left < right:
-        for i in range(right - left):
+        for a in range(right - left):
             top = left
             bottom = right
-            
-            topLeft = matrix[top][left + i]
-            matrix[top][left + i] = matrix[bottom - i][left]
-            matrix[bottom - i][left] = matrix[bottom][right - i]
-            matrix[bottom][right - i] = matrix[top + i][right]
-            matrix[top + i][right] = topLeft
-
+            # Rotate each outer element
+            topLeft = matrix[top][left + a]
+            matrix[top][left + a] = matrix[bottom - a][left]
+            matrix[bottom - a][left] = matrix[bottom][right - a]
+            matrix[bottom][right - a] = matrix[top + a][right]
+            matrix[top + a][right] = topLeft
         right -= 1
         left += 1
