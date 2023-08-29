79a48eafd8cd9cf18480b77b2288ed8139314500
Moses-Onche <onchemoses38@gmail.com>
2023-08-29 23:50:15 +0100

    Island perimiter task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,31 +5,27 @@
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in the grid.
-    
     Args:
         grid (list of list of int): The grid representing the island.
-
     Returns:
         int: The perimeter of the island.
     """
     if not grid:
         return 0
-    
+
     rows = len(grid)
     cols = len(grid[0])
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Add 4 sides for the land cell
-                
-                # Check left neighbor
+                perimeter += 4
+
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2  # Subtract 2 for the shared side
-                
-                # Check top neighbor
+                    perimeter -= 2
+
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2  # Subtract 2 for the shared side
-                    
+                    perimeter -= 2
+
     return perimeter
